a47623f55505b54368f320f975b5fa75aaae1d99
David Yan <idontwanttotalk@gmail.com>
2017-08-24 14:05:34 -0400

    cci1.2

---
+++ CCI1.2/src/stringPerm.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class stringPerm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+        String y = sc.next();
+        boolean b = strCmp(x,y);
+        System.out.println(b);
+    }
+    public static boolean strCmp(String s, String t){
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        return sort(t).equals(sort(t));
+    }
+
+    public static String sort(String s){
+        char[] schar = s.toCharArray();
+        Arrays.sort(schar);
+        return new String(schar);
+    }
+}
+
